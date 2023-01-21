f0a34bd1c2204c5434f761a7d1ed7ed041c3a0ed
mrumbra361 <122780690+mrumbra361@users.noreply.github.com>
2023-01-21 14:23:17 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,23 +6,46 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 function Mover_Pelota () {
-	
+    basic.pause(200)
+    pelota.move(1)
+    pelota.ifOnEdgeBounce()
+    if (pelota.isTouching(paleta_1) && pelota.isTouching(paleta_2)) {
+        pelota.set(LedSpriteProperty.Direction, 0)
+        puntos += 1
+        if (azar == true) {
+            pelota.turn(Direction.Left, 45)
+        } else {
+            pelota.turn(Direction.Right, 45)
+        }
+        azar = Math.randomBoolean()
+    } else {
+        if (pelota.get(LedSpriteProperty.Y) == 4) {
+            basic.clearScreen()
+            basic.showString("GAME OVER")
+            basic.showString("PUNTOS:")
+            basic.showNumber(puntos)
+        }
+    }
 }
 input.onButtonPressed(Button.B, function () {
     if (Posición_Paleta > 3) {
-        paleta_2.move(-1)
-        paleta_1.move(-1)
+        paleta_2.move(1)
+        paleta_1.move(1)
         Posición_Paleta = 1
     }
 })
+let azar = false
+let puntos = 0
 let Posición_Paleta = 0
+let pelota: game.LedSprite = null
 let paleta_2: game.LedSprite = null
 let paleta_1: game.LedSprite = null
 paleta_1 = game.createSprite(2, 4)
 paleta_2 = game.createSprite(3, 4)
-let pelota = game.createSprite(2, 0)
+pelota = game.createSprite(2, 0)
 Posición_Paleta += 2
-Posición_Paleta.turn(Direction.Right, 90)
+puntos = 0
+pelota.turn(Direction.Right, 90)
 basic.forever(function () {
     Mover_Pelota()
 })
